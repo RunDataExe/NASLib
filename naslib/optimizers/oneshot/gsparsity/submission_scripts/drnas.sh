0336ebb80a467227ec335874db898a6c808f0516
Ruben Weber <ruben.weber@students.uni-mannheim.de>
2025-04-07 08:12:21 +0200

    created the submission scripts for the verification of the methods

---
+++ naslib/optimizers/oneshot/gsparsity/submission_scripts/drnas.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --time=48:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=gpu_4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+#SBATCH --cpus-per-task=8
+#SBATCH --job-name=drnas_original
+#SBATCH --output=naslib/optimizers/oneshot/gsparsity/run/drnas_nas_original.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=ruben.weber@students.uni-mannheim.de
+
+python naslib/optimizers/oneshot/gsparsity/configurator.py \
+    --optimizer drnas \
+    --search_space nasbench301 \
+    --dataset cifar10 \
+    --seed 42 \
+    --out_dir naslib/optimizers/oneshot/gsparsity/test
---
+++ naslib/optimizers/oneshot/gsparsity/submission_scripts/gsparses.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --time=48:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=gpu_4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+#SBATCH --cpus-per-task=8
+#SBATCH --job-name=group_sparsity_nas/gs_original
+#SBATCH --output=naslib/optimizers/oneshot/gsparsity/run/group_sparsity_nas_original.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=ruben.weber@students.uni-mannheim.de
+
+python naslib/optimizers/oneshot/gsparsity/configurator.py \
+    --optimizer gsparsity \
+    --search_space nasbench301 \
+    --dataset cifar10 \
+    --seed 42 \
+    --out_dir naslib/optimizers/oneshot/gsparsity/test

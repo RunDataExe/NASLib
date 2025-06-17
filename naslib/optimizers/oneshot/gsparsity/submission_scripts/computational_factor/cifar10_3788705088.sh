698cfc46ef9a6a74f1e2884ae1ef0b064a1a925c
ruweber@uni-mannheim.de <ma_ruweber@uc3n991.localdomain>
2025-06-17 14:40:23 +0200

    created slurm scripts for the generated seeds for each dataset to infer the computational factor per dataset

---
+++ naslib/optimizers/oneshot/gsparsity/submission_scripts/computational_factor/cifar10_3788705088.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --time=01:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=gpu_a100_il
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+#SBATCH --cpus-per-task=8
+#SBATCH --job-name=comp_factor_cifar10_s2152435495
+#SBATCH --output=experiments/comp_factor_naslib/cifar10/slurm_comp_factor_cifar10_s2152435495_%j.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=ruben.weber@students.uni-mannheim.de
+
+# Ensure the output directory for SLURM logs exists
+mkdir -p naslib/optimizers/oneshot/gsparsity/submission_scripts/computational_factor/cifar10
+
+python naslib/utils/computational_factor.py \
+    --dataset cifar10 \
+    --num_archs 1 \
+    --epochs 200 \
+    --batch_size 256 \
+    --seed 3788705088 \
+    --out_dir naslib/optimizers/oneshot/gsparsity/submission_scripts/computational_factor \
+    --log_freq 50
---
+++ naslib/optimizers/oneshot/gsparsity/submission_scripts/computational_factor/imagenet_2152435495.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --time=02:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=gpu_a100_il
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+#SBATCH --cpus-per-task=8
+#SBATCH --job-name=comp_factor_imagenet16_120
+#SBATCH --output=experiments/comp_factor_naslib/ImageNet16-120/slurm_comp_factor_imagenet16_120_%j.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=ruben.weber@students.uni-mannheim.de
+
+# Ensure the output directory for SLURM logs exists
+mkdir -p naslib/optimizers/oneshot/gsparsity/submission_scripts/computational_factor/ImageNet16-120
+
+python naslib/utils/computational_factor.py \
+    --dataset ImageNet16-120 \
+    --num_archs 10 \
+    --epochs 200 \
+    --batch_size 256 \
+    --seed 2152435495 \
+    --out_dir naslib/optimizers/oneshot/gsparsity/submission_scripts/computational_factor \
+    --log_freq 50

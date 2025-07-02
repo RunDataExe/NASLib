e534ed4ad22da0827a727fd0a73c1f8d501bd93c
ruweber@uni-mannheim.de <ma_ruweber@uc3n990.localdomain>
2025-07-02 10:03:53 +0200

    created submission scripts to check if the results of inverted bananas and self training inverted bananas match

---
+++ naslib/optimizers/oneshot/gsparsity/submission_scripts/self_training_bananas_verification/self_training_inverted_bananas_cifar10_1544457859.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --time=04:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=gpu_a100_il
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+#SBATCH --cpus-per-task=16
+#SBATCH --job-name=stib_cifar10_1544457859
+#SBATCH --output=naslib/optimizers/oneshot/gsparsity/submission_scripts/self_training_bananas_verification/slurm_logs/cifar10/slurm_stib_cifar10_1544457859_%j.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=ruben.weber@students.uni-mannheim.de
+
+python naslib/optimizers/oneshot/gsparsity/configurator.py \
+    --optimizer self_training_inverted_bananas \
+    --search_space nasbench201 \
+    --dataset cifar10 \
+    --seed 1544457859 \
+    --out_dir naslib/optimizers/oneshot/gsparsity/submission_scripts/self_training_bananas_verification \
+    --search_epochs 3 \
+    --eval_epochs 1 \
+    --resume True
+
+# For a fair comparison, ensure the following hyperparameters are set in the configurator.py:
+# "self_training_inverted_bananas": {
+#     "search": {
+#         "checkpoint_freq": 1,
+#         "epochs": search_epochs,
+#         "k": 10,
+#         "num_init": 10,
+#         "num_ensemble": 5,
+#         "predictor_type": "mlp",
+#         "acq_fn_type": "its",
+#         "acq_fn_optimization": "mutation",
+#         "encoding_type": "path",
+#         "num_arches_to_mutate": 1,
+#         "max_mutations": 1,
+#         "num_candidates": 100,
+#         "train_epochs": 200,
+#         "use_real_time": True,
+#     },
+# }
+# "inverted_bananas": {
+#     "search": {
+#         "checkpoint_freq": 1,
+#         "epochs": search_epochs,
+#         "k": 10,
+#         "num_init": 10,
+#         "num_ensemble": 5,
+#         "predictor_type": "mlp",
+#         "acq_fn_type": "its",
+#         "acq_fn_optimization": "mutation",
+#         "encoding_type": "path",
+#         "num_arches_to_mutate": 1,
+#         "max_mutations": 1,
+#         "num_candidates": 100,
+#     },
+# }

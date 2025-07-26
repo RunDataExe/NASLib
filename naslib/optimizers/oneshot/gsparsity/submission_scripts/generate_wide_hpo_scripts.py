409c57883541480bded553dfea6919facef78e28
ruweber@uni-mannheim.de <ma_ruweber@uc3n991.localdomain>
2025-07-26 10:54:43 +0200

    fixed generation script to also incoporate zcp method and use the right hpo scirpt

--- naslib/optimizers/oneshot/gsparsity/submission_scripts/generate_wide_hpo_scripts.py
+++ naslib/optimizers/oneshot/gsparsity/submission_scripts/generate_wide_hpo_scripts.py
@@ -1,26 +1,51 @@
 import os
 
 
-def generate_slurm_script(optimizer, dataset, seed):
-    script_content = f"""#!/bin/bash
-#SBATCH --time=5:30:00
+def generate_slurm_script(optimizer, dataset, seed, zcp_method):
+    if zcp_method:
+        script_content = f"""#!/bin/bash
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=gpu_a100_il
 #SBATCH --gres=gpu:1
 #SBATCH --mem=40G
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 #SBATCH --job-name=wide_hpo_{optimizer}_{dataset}_{seed}
-#SBATCH --output=naslib/optimizers/oneshot/gsparsity/results/{optimizer}/{dataset}/slurm/wide_hpo_{seed}_%j.out
+#SBATCH --output=naslib/optimizers/oneshot/gsparsity/results/slurm/wide_hpo_{optimizer}_{dataset}_{seed}_%j.out
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=ruben.weber@students.uni-mannheim.de
 
-python naslib/optimizers/oneshot/gsparsity/wide_hpo_search_space_configurator_loss_based.py \\
+python naslib/optimizers/oneshot/gsparsity/high_budget_wide_hpo_search_space_configurator_val_acc_based.py \\
     --optimizer {optimizer} \\
     --search_space nasbench201 \\
     --dataset {dataset} \\
     --seed {seed} \\
-    --out_dir naslib/optimizers/oneshot/gsparsity/results/{optimizer}/{dataset}/wide_hpo \\
+    --resume True \\
+    --zcp_method {zcp_method} \\
+    --out_dir naslib/optimizers/oneshot/gsparsity/results \\
+"""
+    else:
+        script_content = f"""#!/bin/bash
+#SBATCH --time=48:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=gpu_a100_il
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+#SBATCH --cpus-per-task=32
+#SBATCH --job-name=wide_hpo_{optimizer}_{dataset}_{seed}
+#SBATCH --output=naslib/optimizers/oneshot/gsparsity/results/slurm/wide_hpo_{optimizer}_{dataset}_{seed}_%j.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=ruben.weber@students.uni-mannheim.de
+
+python naslib/optimizers/oneshot/gsparsity/high_budget_wide_hpo_search_space_configurator_val_acc_based.py \\
+    --optimizer {optimizer} \\
+    --search_space nasbench201 \\
+    --dataset {dataset} \\
+    --seed {seed} \\
+    --resume True \\
+    --out_dir naslib/optimizers/oneshot/gsparsity/results \\
 """
     return script_content
 
@@ -29,22 +54,33 @@ def main():
     optimizers = [
         "gsparsity",
         "zcp_gsparsity",
-        "inverted_bananas_gsparsity",
-        "inverted_bananas_zcp_gsparsity",
+        "zcp-pre_gsparsity",
+        "zcp-pre_zcp_gsparsity",
+        # "inverted_bananas_gsparsity",
+        # "inverted_bananas_zcp_gsparsity",
     ]
-    datasets = ["cifar10", "cifar100", "imagenet"]
+    # datasets = ["cifar10", "cifar100", "ImageNet16-120"]
+    datasets = ["cifar100", "ImageNet16-120"]
+
+    zcp_method = ["jacov", "params", "synflow"]
+
     seed = 2152435495
 
     os.makedirs("output", exist_ok=True)
 
     for optimizer in optimizers:
         for dataset in datasets:
-            script = generate_slurm_script(optimizer, dataset, seed)
-            output_dir = f"naslib/optimizers/oneshot/gsparsity/submission_scripts/{optimizer}/{dataset}"
+            output_dir = f"naslib/optimizers/oneshot/gsparsity/submission_scripts/wide_hpo/{optimizer}"
             os.makedirs(output_dir, exist_ok=True)
-            script_filename = f"{output_dir}/wide_hpo_{seed}.sh"
-            with open(script_filename, "w") as f:
-                f.write(script)
+            if "zcp_gsparsity" in optimizer:
+                for zcp in zcp_method:
+                    script_filename = f"{output_dir}/{dataset}_{zcp}_{seed}.sh"
+                    with open(script_filename, "w") as f:
+                        f.write(generate_slurm_script(optimizer, dataset, seed, zcp))
+            else:
+                script_filename = f"{output_dir}/{dataset}_{seed}.sh"
+                with open(script_filename, "w") as f:
+                    f.write(generate_slurm_script(optimizer, dataset, seed, None))
 
 
 if __name__ == "__main__":

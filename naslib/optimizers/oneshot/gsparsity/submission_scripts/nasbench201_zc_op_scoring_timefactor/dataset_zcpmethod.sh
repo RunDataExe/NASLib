9b10cf60048e63734ab4fa6401ece77239f00ba8
ruweber@uni-mannheim.de <ma_ruweber@uc3n991.localdomain>
2025-08-14 09:26:06 +0200

    made the slurm script coherent to the called script

--- naslib/optimizers/oneshot/gsparsity/submission_scripts/nasbench201_zc_op_scoring_timefactor/dataset_zcpmethod.sh
+++ naslib/optimizers/oneshot/gsparsity/submission_scripts/nasbench201_zc_op_scoring_timefactor/dataset_zcpmethod.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --time=05:00:00
+#SBATCH --time=06:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=gpu_a100_il
 #SBATCH --gres=gpu:1
 #SBATCH --mem=40G
 #SBATCH --cpus-per-task=32
-#SBATCH --job-name=zc_op_scoring_nb201
-#SBATCH --output=naslib/optimizers/oneshot/gsparsity/submission_scripts/nasbench201_zc_op_scoring_timefactor/slurm_logs/slurm_op_precompute_%A_%a.out
-#SBATCH --array=0-8
+#SBATCH --job-name=opzcp_precompute_verify_nb201
+#SBATCH --output=naslib/optimizers/oneshot/gsparsity/submission_scripts/nasbench201_zc_op_scoring_timefactor/slurm_logs/slurm_precompute_verify_%A_%a.out
+#SBATCH --array=0-$(($((${#DATASETS[@]}*${#METHODS[@]}))-1))
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=ruben.weber@students.uni-mannheim.de
 
@@ -26,10 +26,12 @@ MIDX=$(( SLURM_ARRAY_TASK_ID % ${#METHODS[@]} ))
 DATASET=${DATASETS[$DIDX]}
 ZCP_METHOD=${METHODS[$MIDX]}
 
-echo "Precomputing op ZCP scores: dataset=${DATASET}, method=${ZCP_METHOD}, seed=${SEED}"
+echo "Precompute and verify op ZCPs: dataset=${DATASET}, method=${ZCP_METHOD}, seed=${SEED}"
 
-python naslib/optimizers/oneshot/gsparsity/zc_score_op_time_saver.py \
+python -u naslib/optimizers/oneshot/gsparsity/precompute_and_verify_op_zcp.py \
   --dataset "${DATASET}" \
   --zcp_method "${ZCP_METHOD}" \
   --seed "${SEED}" \
-  --out_dir naslib/optimizers/oneshot/gsparsity/submission_scripts/nasbench201_zc_op_scoring_timefactor
+  --out_base_dir naslib/optimizers/oneshot/gsparsity/submission_scripts/nasbench201_zc_op_scoring_timefactor \
+  --arch_scores_dir naslib/optimizers/oneshot/gsparsity/submission_scripts/nasbench201_zc_scoring_timefactor \
+  --prune_topk 750

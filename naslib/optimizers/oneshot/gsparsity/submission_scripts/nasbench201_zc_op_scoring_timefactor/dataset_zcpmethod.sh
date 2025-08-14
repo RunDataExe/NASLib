6b326ebf90b11cad0b2cda715b6e4aaf274212fa
ruweber@uni-mannheim.de <ma_ruweber@uc3n991.localdomain>
2025-08-14 10:19:31 +0200

    fixed which script is called and the array size

--- naslib/optimizers/oneshot/gsparsity/submission_scripts/nasbench201_zc_op_scoring_timefactor/dataset_zcpmethod.sh
+++ naslib/optimizers/oneshot/gsparsity/submission_scripts/nasbench201_zc_op_scoring_timefactor/dataset_zcpmethod.sh
@@ -8,7 +8,7 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --job-name=opzcp_precompute_verify_nb201
 #SBATCH --output=naslib/optimizers/oneshot/gsparsity/submission_scripts/nasbench201_zc_op_scoring_timefactor/slurm_logs/slurm_precompute_verify_%A_%a.out
-#SBATCH --array=0-6
+#SBATCH --array=0-5
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=ruben.weber@students.uni-mannheim.de
 
@@ -20,6 +20,12 @@ DATASETS=("cifar100" "ImageNet16-120")
 METHODS=("params" "jacov" "synflow")
 SEED=2152435495
 
+TOTAL=$(( ${#DATASETS[@]} * ${#METHODS[@]} ))
+if (( SLURM_ARRAY_TASK_ID >= TOTAL )); then
+  echo "SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID out of range (0..$((TOTAL-1))). Exiting."
+  exit 0
+fi
+
 DIDX=$(( SLURM_ARRAY_TASK_ID / ${#METHODS[@]} ))
 MIDX=$(( SLURM_ARRAY_TASK_ID % ${#METHODS[@]} ))
 
@@ -28,7 +34,7 @@ ZCP_METHOD=${METHODS[$MIDX]}
 
 echo "Precompute and verify op ZCPs: dataset=${DATASET}, method=${ZCP_METHOD}, seed=${SEED}"
 
-python -u naslib/optimizers/oneshot/gsparsity/precompute_and_verify_op_zcp.py \
+python -u naslib/optimizers/oneshot/gsparsity/zc_score_op_time_saver.py \
   --dataset "${DATASET}" \
   --zcp_method "${ZCP_METHOD}" \
   --seed "${SEED}" \

e31d7fb2c028b405123b55a6621674dbb0f3c5ed
RubenWeber <ruben.weber@students.uni-mannheim.de>
2025-11-23 12:15:02 +0100

    changed default

--- naslib/optimizers/oneshot/gsparsity/table_scripts/final_hp_setting_table_sva_shifted.py
+++ naslib/optimizers/oneshot/gsparsity/table_scripts/final_hp_setting_table_sva_shifted.py
@@ -46,7 +46,7 @@ def parse_args() -> argparse.Namespace:
     )
     parser.add_argument(
         "--output",
-        default="naslib/optimizers/oneshot/gsparsity/table_scripts/tables/sva_final_hp_setting.xlsx",
+        default="naslib/optimizers/oneshot/gsparsity/table_scripts/tables/sva/final_hp_setting.xlsx",
         help="Excel file to create.",
     )
     # NEW: durations-dir used for time-shift offset (same default as check script)

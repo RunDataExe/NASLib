a9b26807784bad145aca02d08068a004b9bec409
RubenWeber <ruben.weber@students.uni-mannheim.de>
2025-12-05 15:08:22 +0100

    changed default folder again

--- naslib/optimizers/oneshot/gsparsity/plotting_scripts/hpo_plotting_sva_shifted.py
+++ naslib/optimizers/oneshot/gsparsity/plotting_scripts/hpo_plotting_sva_shifted.py
@@ -1258,7 +1258,7 @@ def main():
     )
     ap.add_argument(
         "--out_dir",
-        default="naslib/optimizers/oneshot/gsparsity/plotting_scripts/sva_hpo",
+        default="naslib/optimizers/oneshot/gsparsity/plotting_scripts/plots/sva_hpo",
     )
     ap.add_argument("--search_space", default="nasbench201")
     ap.add_argument(
